fdfb1867c26c388988dbb41255e76f4f788e6b7b
Jonathan <jonathan.hartmayer@gmail.com>
2019-02-26 15:08:06 -0800

    Create color palette

---
+++ frontend/goblin.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const rootEl = document.getElementById('root');
+    ReactDOM.render(<h2>Welcome to Goblin</h2>, rootEl);
+});
