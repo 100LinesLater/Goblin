96d147ed880980577085aa52cf9f176f057ae5bf
Jonathan <jonathan.hartmayer@gmail.com>
2019-03-02 22:52:40 -0800

    Create new api routes for stocks, portfolios, and transactions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do 
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
+    resources :stocks, only: [:create]
+    resources :portfolios, only: [:create, :update]
+    resources :transactions, only: [:create]
   end
   root to: 'static_pages#root'
 end
