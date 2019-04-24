8cdefb7a0da2f3fcb8d6e51ea14724f0f09cb6f1
Yoni Hartmayer <yonihart416@gmail.com>
2019-04-23 19:05:20 -0700

    Add endpoints from worldtradingdata.com to external api util

--- frontend/util/external_api_util.js
+++ frontend/util/external_api_util.js
@@ -1,7 +1,27 @@
 const pathStart = "https://api.iextrading.com/1.0";
+const pathStartWorld = "https://www.worldtradingdata.com/api/v1";
+const apiToken = "hY9EnOWqeuof9zXhexjNSPdqBX3Gjhf7eT9v4lw3kxct6yLEHPIPTg9rfvn4";
 
 // Stock Chart Data 
-// Tags: 1d, 1m, 3m, 1y, 5y
+export const fetchDailyChart = (ticker) => (
+    $.ajax({
+        method: 'GET',
+        url: `${pathStartWorld}/intraday?symbol=${ticker}&range=1&interval=5&api_token=${apiToken}`
+    })
+);
+export const fetchWeeklyChart = (ticker) => (
+    $.ajax({
+        method: 'GET',
+        url: `${pathStartWorld}/intraday?symbol=${ticker}&range=7&interval=30&api_token=${apiToken}`
+    })
+);
+export const fetchHistoricalChart = (ticker) => (
+    $.ajax({
+        method: 'GET',
+        url: `${pathStartWorld}/history?symbol=${ticker}&date_from=2018-01-01&date_to=2019-12-31&sort=newest&api_token=${apiToken}`
+    })
+);
+
 export const fetchChart = (ticker, timeTag) => (
     $.ajax({
         method: 'GET',
