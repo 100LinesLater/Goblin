dcc139092e1723716cd07f425c22a70d89071962
Yoni Hartmayer <yonihart416@gmail.com>
2019-06-19 14:12:32 -0700

    Delete account link in header

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -14,10 +14,6 @@ const Header = (props) => (
             <NavLink className="header-home"
                 to="/">Home
                 </NavLink>
-            <NavLink className="header-account"
-                to="/account">
-                Account
-                </NavLink>
             <button
                 className="home-logout-button"
                 onClick={props.logout}>Log Out
