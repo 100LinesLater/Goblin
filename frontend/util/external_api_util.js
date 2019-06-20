c58dbef680d2f85df8a073d605d64e448408811c
Yoni Hartmayer <yonihart416@gmail.com>
2019-06-20 16:29:17 -0700

    Remove other api options

--- frontend/util/external_api_util.js
+++ frontend/util/external_api_util.js
@@ -1,27 +1,9 @@
 const pathStart = "https://api.iextrading.com/1.0";
+const apiToken = "";
 const pathStartWorld = "https://www.worldtradingdata.com/api/v1";
-const apiToken = "hY9EnOWqeuof9zXhexjNSPdqBX3Gjhf7eT9v4lw3kxct6yLEHPIPTg9rfvn4";
+const worldApiToken = "hY9EnOWqeuof9zXhexjNSPdqBX3Gjhf7eT9v4lw3kxct6yLEHPIPTg9rfvn4";
 
-// Stock Chart Data 
-export const fetchDailyChart = (ticker) => (
-    $.ajax({
-        method: 'GET',
-        url: `${pathStartWorld}/intraday?symbol=${ticker}&range=1&interval=5&api_token=${apiToken}`
-    })
-);
-export const fetchWeeklyChart = (ticker) => (
-    $.ajax({
-        method: 'GET',
-        url: `${pathStartWorld}/intraday?symbol=${ticker}&range=7&interval=30&api_token=${apiToken}`
-    })
-);
-export const fetchHistoricalChart = (ticker) => (
-    $.ajax({
-        method: 'GET',
-        url: `${pathStartWorld}/history?symbol=${ticker}&date_from=2018-01-01&date_to=2019-12-31&sort=newest&api_token=${apiToken}`
-    })
-);
-
+// Stock Chart Data
 export const fetchChart = (ticker, timeTag) => (
     $.ajax({
         method: 'GET',
@@ -53,6 +35,6 @@ export const fetchNews = () => (
 export const fetchSearchResults = (term) => (
     $.ajax({
         method: 'GET',
-        url: `${pathStartWorld}/stock_search?search_term=${term}&limit=5&page=1&api_token=${apiToken}`
+        url: `${pathStartWorld}/stock_search?search_term=${term}&limit=5&page=1&api_token=${worldApiToken}`
     })
 );
