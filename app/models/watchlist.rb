75f029cb7b9a72bc894f63b5f850698a999c6b0c
Yoni Hartmayer <yonihart416@gmail.com>
2019-08-22 17:34:52 -0700

    Create watchlist associations

--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -10,4 +10,14 @@
 #
 
 class Watchlist < ApplicationRecord
+  validates: user_id, stock_id, presence: true;
+
+  belongs_to :user,
+    foreign_key: :user_id,
+    class_name: :User
+
+  belongs_to :stock,
+    foreign_key: :stock_id,
+    class_name: :Stock 
+    
 end
