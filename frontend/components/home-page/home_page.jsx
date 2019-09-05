0439cfe002fb94535d0a4db90215b4b88802ab43
Yoni Hartmayer <yonihart416@gmail.com>
2019-09-05 14:44:34 -0700

    Render portfolio chart with chart data got by formula

--- frontend/components/home-page/home_page.jsx
+++ frontend/components/home-page/home_page.jsx
@@ -13,12 +13,10 @@ class HomePage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: null,
-            price: 0,
             color: null,
-            ticker: 'goog',
             interval: '1m',
             currentPrice: 10000,
+            stocksInTransactions: {},
             allTransactionStockChartData: [],
             portfolioPriceData: [],
         };
@@ -29,28 +27,33 @@ class HomePage extends React.Component {
         this.props.fetchWatchlists();
         this.props.fetchTransactions();
         this.props.fetchNews();
-        // Delete fetchChart once transaction array is in place
-        fetchChart(this.state.ticker, this.state.interval)
-            .then(res => this.setState({ data: res }))
-            .then(res => this.setState({
-                color: [
-                    (this.state.data[this.state.data.length - 1].close < this.state.data[0].close) ?
-                        "#f1563a" : "#30cd9a"]
-            }
-            ));
     }
 
     componentDidUpdate(_prevProps, prevState) {
         const { transactions } = this.props;
+        const { allTransactionStockChartData, 
+            stocksInTransactions, portfolioPriceData} = this.state;
         if (transactions.length && _prevProps.transactions.length !== transactions.length) {
             this.portfolioSetup();
         }
-        if (this.state.allTransactionStockChartData.length && 
-            this.state.allTransactionStockChartData.length !== 
-            prevState.allTransactionStockChartData.length) {
+        if (allTransactionStockChartData.length && 
+            allTransactionStockChartData.length === stocksInTransactions.size &&
+            prevState.allTransactionStockChartData.length === stocksInTransactions.size - 1) {
                 this.setState({
                     portfolioPriceData: this.fillPortfolioPriceData(transactions)
                 });
+        }
+        if (portfolioPriceData.length &&
+            portfolioPriceData.length !== prevState.portfolioPriceData.length) {
+            this.setState({
+                color: [
+                (portfolioPriceData[portfolioPriceData.length - 1].close < 
+                    portfolioPriceData[0].close) ?
+                        "#f1563a" : "#30cd9a"
+                ], 
+                currentPrice: portfolioPriceData[portfolioPriceData.length - 1].close
+            });
+            console.log(this.state.portfolioPriceData);
         }
         // portfolio total price = last entry of portfolio chart
         if (prevState.interval !== this.state.interval) {
@@ -86,6 +89,7 @@ class HomePage extends React.Component {
         let stocksInTransactions = new Set(
             this.props.transactions.map(tx => tx.ticker)
         );
+        this.setState({stocksInTransactions});
         stocksInTransactions.forEach(stock =>
             fetchChartWithoutChartInterval(stock, this.state.interval)
                 .then(res => this.setState({ allTransactionStockChartData:
@@ -95,30 +99,11 @@ class HomePage extends React.Component {
         );
     }
 
-    // portfolioFormula() {
-    //     const stockRecord = {}; // ex: GOOG: 5, MSFT: 14
-    //     const stocksInTransactions = new Set(
-    //         this.props.transactions.map(tx => tx.ticker)
-    //     );
-    //     let allTxStockChartData;
-    //     //     = fillTransactionStockData(stocksInTransactions,
-    //     //         this.state.interval
-    //     // );
-    //     const datesInRange = allTxStockChartData[0].data.map(
-    //         data => data.transaction_date
-    //     );
-    //     const portfolioPriceData = fillPortfolioPriceData(
-    //         this.props.transactions, datesInRange, allTxStockChartData, stockRecord
-    //     );
-    //     this.setState({portfolioPriceData});
-    // }
-
-
     fillPortfolioPriceData(txs) {
         const datesInRange = this.state.allTransactionStockChartData[0].data.map(
             data => data.date
         );
-        // Construct stockHash from transactions that came before date range
+
         const stockHash = {}; // ex: GOOG: 5, MSFT: 14
         let i = 0;
         
@@ -173,16 +158,18 @@ class HomePage extends React.Component {
     }
 
     render() {
+        const {portfolios, watchlists} = this.props;
+        const {currentPrice, portfolioPriceData, color} = this.state;
         return (
             <div className="home-page-main">
 
                 <div className="portfolio-chart-main">
                     <div className="portfolio-chart-price">
-                        <h1>${this.state.currentPrice.toFixed(2)}</h1>
+                        <h1>${currentPrice.toFixed(2)}</h1>
                     </div>
                     <PortfolioChart className="portfolio-chart-chart"
-                        data={this.state.data} 
-                        color={this.state.color}
+                        data={portfolioPriceData} 
+                        color={color}
                     />
                     <div className="portfolio-chart-time-tags">
                         <li><a onClick={() => this.onChangeInterval('1m')}>{'1M'}</a></li>
@@ -196,7 +183,7 @@ class HomePage extends React.Component {
                     <div className="portfolio-sidebar-title">
                         <p>Stocks</p>
                     </div>
-                    {this.props.portfolios.filter( port => 
+                    {portfolios.filter( port => 
                         port.num_shares > 0
                     ).map( (port, idx) => {
                         return (
@@ -209,7 +196,7 @@ class HomePage extends React.Component {
                     <div className="portfolio-sidebar-title">
                         <p>Watchlist</p>
                     </div>
-                    {this.props.watchlists.map( (watch, idx) => {
+                    {watchlists.map( (watch, idx) => {
                         return (
                             <WatchlistItem key={idx}
                             ticker={watch.ticker}
