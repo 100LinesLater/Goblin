2e7917202ed7f1e6c2fe24233758f893025a5f97
Yoni Hartmayer <yonihart416@gmail.com>
2019-09-16 00:07:02 -0700

    Add stock name to props passed down to search filter item

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -7,74 +7,75 @@ import SearchFilterItem from './search_filter_item';
 //     .then(res => console.log(res.data[0].symbol));
 
 class Header extends React.Component { 
-    constructor(props) {
-        super(props);
-        this.state = {
-            searchTerm: "",
-            searchResults: [],
-        };
-        this.handleInputChange = this.handleInputChange.bind(this);
-    }
+  constructor(props) {
+      super(props);
+      this.state = {
+          searchTerm: "",
+          searchResults: [],
+      };
+      this.handleInputChange = this.handleInputChange.bind(this);
+  }
 
-    handleInputChange(e) {
-        e.preventDefault();
-        this.setState({ searchTerm: e.target.value });
-        if (e.target.value !== "") {
-            fetchSearchResults(e.target.value)
-                .then(res => this.setState({searchResults: res.data}));
-        } else {
-            this.setState({searchResults: []});
-        }
-    }
+  handleInputChange(e) {
+      e.preventDefault();
+      this.setState({ searchTerm: e.target.value });
+      if (e.target.value !== "") {
+          fetchSearchResults(e.target.value)
+              .then(res => this.setState({searchResults: res.data}));
+      } else {
+          this.setState({searchResults: []});
+      }
+  }
 
-    render() {
-        return (
-          <div className="nav-bar">
-            <a className="logo-top" onClick={() => (window.location = '/')} />
-            <div className="stock-search">
-              <div className="stock-search-bar">
-                <span className="fa fa-search" />
-                <input
-                  className="search-bar-input"
-                  type="text"
-                  onChange={this.handleInputChange}
-                  placeholder="Search"
-                />
-              </div>
-              {this.state.searchResults.length ? (
-                <ul className="search-filter">
-                  {this.state.searchResults.map((item, idx) => (
-                    <SearchFilterItem
-                      key={idx}
-                      ticker={item.symbol}
-                      exchange={item.stock_exchange_short}
-                      reset={() => this.setState({searchResults: []})}
-                    />
-                  ))}
-                </ul>
-              ) : (
-                <></>
-              )}
-            </div>
-            <span className="header-links">
-              <a
-                className="header-home"
-                onClick={() => (window.location = '/')}>
-                Home
-              </a>
-              <button
-                className="home-logout-button"
-                onClick={this.props.logout}>
-                Log Out
-              </button>
-
-              {/* <h3 className="header-name">
-                        Hi, {props.currentUser.first_name} {props.currentUser.last_name}!
-                        </h3>  */}
-            </span>
+  render() {
+    return (
+      <div className="nav-bar">
+        <a className="logo-top" onClick={() => (window.location = '/')} />
+        <div className="stock-search">
+          <div className="stock-search-bar">
+            <span className="fa fa-search" />
+            <input
+              className="search-bar-input"
+              type="text"
+              onChange={this.handleInputChange}
+              placeholder="Search"
+            />
           </div>
-        );
-    }
+          {this.state.searchResults.length ? (
+            <ul className="search-filter">
+              {this.state.searchResults.map((item, idx) => (
+                <SearchFilterItem
+                  key={idx}
+                  ticker={item.symbol}
+                  name={item.name}
+                  exchange={item.stock_exchange_short}
+                  reset={() => this.setState({searchResults: []})}
+                />
+              ))}
+            </ul>
+          ) : (
+            <></>
+          )}
+        </div>
+        <span className="header-links">
+          <a
+            className="header-home"
+            onClick={() => (window.location = '/')}>
+            Home
+          </a>
+          <button
+            className="home-logout-button"
+            onClick={this.props.logout}>
+            Log Out
+          </button>
+
+          {/* <h3 className="header-name">
+                    Hi, {props.currentUser.first_name} {props.currentUser.last_name}!
+                    </h3>  */}
+        </span>
+      </div>
+    );
+  }
 }
 
 export default Header;
